ecb1530cc9b2b5eebd6ea3dff8d977a0fa5f8d6a
magjene <pdfjene@mail.ru>
2022-09-29 22:54:03 +0300

    Added DOC

--- Password Hacker/task/hacking/hack.py
+++ Password Hacker/task/hacking/hack.py
@@ -1,30 +1,31 @@
 """
 Project: Password Hacker
-Stage 2/5: Simple brute force
+Stage 3/5: Smarter, dictionary-based brute force
 
 
 Description
-The admin noticed someone sneaking around the site with admin rights and came up with a password. Now to log in as an admin, you need to enter the password first. Maybe the admin has set a relatively easy and short password so that it is easy to remember? Let's try to brute force all possible passwords to enter the site!
+Looks like you can already call yourself a hacker! However, the situation gets more complicated: the admin improves the server and our simple brute force attack is no longer working. Well, this shouldn't hold you back: you can provide your program with a prepared dictionary of typical passwords (it was generated using a database with over a million real-life passwords).
 
-So far the program is very simplistic: it’s time to improve it so that it can generate different variants of the password and then try each one. The admin of the server doesn’t hide the information that passwords vary in length and may include letters from a to z and numbers from 0 to 9. You should start with a,b,c,....,z,0,1,..aa,ab,ac,ad and continue until your password is correct. The itertools.product() function can help you here. It’s very important to try all the variants of every length because otherwise your program risks never finding the password!
+That's not all: the admin decided to outsmart us and changed the case of some letters in the new password so that we could not crack it using the password dictionary. Let's outsmart the admin and try all possible combinations of upper and lower case for each letter for all words of the password dictionary. We won't have to try too much since for a 6-letter word you'll get only 64 possible combinations.
 
-If the password is correct, you will receive the Connection success! message. Otherwise, you will see the Wrong password! message. The server cannot receive more than a million attempts, so if your program works indefinitely, you will see the unfortunate message Too many attempts.
+Now you need not only to try each element of the dictionary but also change the case of some letters to find the correct password.
+
+This has increased the time of hacking greatly, so using brute force is probably not an option. Use the dictionary of standard passwords, and do not forget to try changing the cases of different letters. For example, there is the word ‘qwerty’ in the dictionary, but the cunning admin sets it to ‘qWeRTy’. Your program should make it possible to hack such passwords, too.
 
 Objectives
 In this stage, you should write a program that:
 
 Parses the command line and gets two arguments that are IP address and port.
-Tries different passwords until it finds the correct one.
+Finds the correct password using the list of typical passwords.
 Prints the password it found.
-Note that you can connect to the server only once and then send messages many times. Don't connect to the server before sending every message.
-
-Also, note that here and throughout the project, the password is different every time you check your code.
+Put the file with typical passwords into your working directory which you can find with a little help of the os module.
+Note that here and throughout the project, the password is different every time you check your code.
 
 Example
 The greater-than symbol followed by a space (> ) represents the user input. Note that it's not part of the input.
 
 > python hack.py localhost 9090
-pass
+qWeRTy
 """
 
 
