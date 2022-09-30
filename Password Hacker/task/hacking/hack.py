3ebe34fc8fbac3de8af20045d0bf4194e00a866a
magjene <pdfjene@mail.ru>
2022-10-01 00:49:27 +0300

    Added DOC

--- Password Hacker/task/hacking/hack.py
+++ Password Hacker/task/hacking/hack.py
@@ -1,56 +1,20 @@
 """
 Project: Password Hacker
-Stage 4/5: Catching exception
+Stage 5/5: Time-based vulnerability
 
 
 Description
-The server is becoming smarter along with your hacking program. Now the admin has implemented a security system by login and password. In order to access the site with admin privileges, you need to know the admin's login and password. Fortunately, we have a dictionary of different logins and a very interesting vulnerability. You need to improve your program once again to hack the new system.
+Your program has successfully hacked the new system! However, you've been spotted: the admin noticed your first failed attempts, found the vulnerability and made a patch. You should overcome this patch and hack the system again. It’s not easy being a hacker!
 
-Also, now the admin has made a complex password that is guaranteed to be absent in the databases since it's randomly generated from several characters.
+The admin has improved the server: the program now catches the exception and sends a simple ‘wrong password’ message to the client even when the real password starts with current symbols.
 
-The server now uses JSON to send messages.
-
-First of all, you should adjust your program so that it can send the combination of login and password in JSON format to the server. Your request should now look like this:
-
-{
-    "login": "admin",
-    "password": "12345678"
-}
-In case of the wrong login, the response you receive looks like this:
-
-{
-    "result": "Wrong login!"
-}
-If you got the login right but failed to find the password, you get this:
-
-{
-    "result": "Wrong password!"
-}
-If some exception happens, you'll see this result:
-
-{
-    "result": "Exception happened during login"
-}
-When you finally succeed in finding both the login and the password, you'll see the following:
-
-{
-    "result": "Connection success!"
-}
-Use the dictionary of typical admin logins. Since you don’t know the login, you should try different variants from the dictionary the same way you did at the previous stage with the passwords.
-
-Use an empty password while searching for the correct login. It matters because you will know that the login is correct the moment you get the ‘wrong password’ result instead of ‘wrong login’.
-As for passwords, they’ve become yet harder, so a simple dictionary is no longer enough. Fortunately, a vulnerability has been found: the ‘exception’ message pops up when the symbols you tried for the password match the beginning of the correct one.
+But here's the thing: the admin probably just caught this exception. We know that catching an exception takes the computer a long time, so there should be a delay in the server response when this exception takes place. You can use it to hack the system: count the time period in which the response comes and find out which starting symbols work out for the password.
 
 Objectives
-Your algorithm is the following:
+In this stage, you should write a program that uses the time vulnerability to find the password.
 
-Try all logins with an empty password.
-When you find the login, try out every possible password of length 1.
-When an exception occurs, you know that you found the first letter of the password.
-Use the found login and the found letter to find the second letter of the password.
-Repeat until you receive the ‘success’ message.
-Finally, your program should print the combination of login and password in JSON format. The examples show two ways of what the output can look like.
-
+Use the list of logins from the previous stage.
+Output the result as you did this in the previous stage.
 Examples
 The greater-than symbol followed by a space (> ) represents the user input. Note that it's not part of the input.
 
@@ -58,13 +22,13 @@ Example 1:
 
 > python hack.py localhost 9090
 {
-    "login" : "superuser",
-    "password" : "aDgT9tq1PU0"
+    "login" : "su",
+    "password" : "fTUe3O99Rre"
 }
 Example 2:
 
 > python hack.py localhost 9090
-{"login": "new_user", "password": "Sg967s"}
+{"login": "admin3", "password": "mlqDz33x"}
 """
 
 
